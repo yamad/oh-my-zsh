cf4556e388e9df3c6f791a395bca08357a796ec1
yamad <jyamada1@gmail.com>
2017-05-30 22:53:34 -0700

    prefer emacsclient for quick starts. prefer nvim.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,7 +1,12 @@
 alias ap='cat >> ~/.reminders'
-alias e='emacs -nw'
+alias e='emacsclient -nw'
 alias lpduplex='lp -o sides=two-sided-long-edge'
 alias pipiu="pip install --install-option='--user'"
 alias pipiu2="pip2 install --install-option='--user'"
 alias ziplist="zipinfo -1"
-alias open="xdg-open"
+alias inkscapeFix='wmctrl -r Inkscape -e 0,2560,1600,1920,1080'
+alias scribus_fix='ps2pdf -dCompressFonts=true -dPDFSETTINGS=/prepress -dEmbedAllFonts'
+alias rtags-capture="intercept-build"
+if type nvim > /dev/null 2>&1; then
+    alias vim='nvim'
+fi
