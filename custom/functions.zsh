27561459f3dcaa89fe50e950744b9524111e998f
yamad <jyamada1@gmail.com>
2017-05-30 22:55:43 -0700

    verbose copy using rsync so progress on long jobs is obvious

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -12,6 +12,11 @@ pomodoro() {
     utimer -c 5m && play ~/downloads/b1_oringz-pack-nine-20.mp3
 }
 
+# verbose copy (rsync backend) -- indicates progress, etc
+cp_v() {
+    rsync -WavP $1 $2
+}
+
 webcam-record() {
     ffmpeg -f video4linux2 -r 30 -b 600k -i /dev/video1 -f alsa -i hw:1,0 -f mp4 $1
 }
